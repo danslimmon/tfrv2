3c47105327a207c545f1468f45a95fda94aea06b
Barry Morrison <barry@hashicorp.com>
2019-09-04 18:40:58 -0500

    what about this?

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
-data "external" "slow-data-source-external" {
-  program = ["curl",  "http://nothing-to-see-here-but-really-this-is-for-mock-games-delete.s3-website-us-west-2.amazonaws.com/"]
+#data "external" "slow-data-source-external" {
+#  program = ["curl",  "http://nothing-to-see-here-but-really-this-is-for-mock-games-delete.s3-website-us-west-2.amazonaws.com/"]
+#}
+
+resource "null_resource" "foobar" {
+  provisioner "local-exec" {
+    command = <<EOT
+      curl http://nothing-to-see-here-but-really-this-is-for-mock-games-delete.s3-website-us-west-2.amazonaws.com/
+    EOT
+  }
 }
+
 resource "random_id" "random" {
   keepers = {
     uuid = "${uuid()}"
