dafeb6c29d8aecaf3d7e8fd160a5908159656336
Dan Slimmon <dan.slimmon@gmail.com>
2019-09-05 16:36:12 +0000

    make it run for longer

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 resource "null_resource" "foobar" {
   provisioner "local-exec" {
     command = <<EOT
-      curl http://nothing-to-see-here-but-really-this-is-for-mock-games-delete.s3-website-us-west-2.amazonaws.com/
+      for i in $(seq 1 300); do curl http://nothing-to-see-here-but-really-this-is-for-mock-games-delete.s3-website-us-west-2.amazonaws.com/; sleep 1; done
     EOT
   }
 }
